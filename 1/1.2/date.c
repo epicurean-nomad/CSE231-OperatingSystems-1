cf7c7a41dc890c8c21a2c1764875500644cd8f6c
Harsh Kumar <harsh19043@iiitd.ac.in>
2020-09-29 19:49:14 +0530

    Ass1: Q2 - Custom Terminal - Implemented date
    Completed basic implementation of all commands

--- 1/1.2/date.c
+++ 1/1.2/date.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
-    printf("Date is printed\n");
+    time_t t ; 
+    struct tm *tmp ; 
+    char MY_TIME[50]; 
+    time( &t );
+
+    tmp = localtime( &t ); 
+    strftime(MY_TIME, sizeof(MY_TIME), "%A %d %B %Y %r %Z", tmp); 
+
+    printf("%s\n", MY_TIME);
 }
